a9422f6b6a9fca7409dcfc418a8a58335ff5f930
CyberXYZD <90124665+CyberXYZD@users.noreply.github.com>
2026-06-23 13:41:55 +0630

    add to the project

--- Scrabble.py
+++ Scrabble.py
@@ -1,22 +1,43 @@
-player1 = input("Player 1: ")
-player2 = input("Player 2: ")
+game_play = True
+while game_play:
+    player1 = input("Player 1: ")
+    player2 = input("Player 2: ")
 
-count1 = 0
-count2 = 0
+    count1 = 0
+    count2 = 0
 
-for p1 in player1:
-    count1 = count1 + 1
-#print(count1)
+    for p1 in player1:
+        count1 = count1 + 1
+    #print(count1)
 
-for p2 in player2:
-    count2 = count2 + 1
-#print(count2)
+    for p2 in player2:
+        count2 = count2 + 1
+    #print(count2)
 
-if count1 == count2:
-    print(f"Tie!")
+    if count1 == count2:
+        print(f"Tie!")
+        game_play = False
 
-elif count1 > count2:
-    print(f"Player1 win!")
 
-else:
-    print(f"Player2 win!")
+    elif count1 > count2:
+        print(f"Player1 win!")
+        game_play = False
+
+
+    else:
+        print(f"Player2 win!")
+        game_play = False
+
+
+    again = True
+
+    while again:
+        play_more = input("Do you want to play again? (y/n)").lower()
+        if play_more == "y":
+            game_play = True
+            again = False
+
+        else:
+            print("Thanks for playing!")
+            again = False
+
